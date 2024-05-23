b4926b4812b216e993dbd0cf10099119c1cba88d
Batuhan Kas <bkas@k2m01s05.42kocaeli.com.tr>
2024-05-23 14:09:04 +0300

    push

--- ex00/src/Zombie.cpp
+++ ex00/src/Zombie.cpp
@@ -6,8 +6,18 @@
 /*   By: bkas <bkas@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 14:04:19 by bkas              #+#    #+#             */
-/*   Updated: 2024/05/23 14:04:59 by bkas             ###   ########.fr       */
+/*   Updated: 2024/05/23 14:08:59 by bkas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Zombie.hpp"
+
+Zombie::Zombie() {}
+
+Zombie::~Zombie() { cout << zName << " is Destroyed." << endl; }
+
+void Zombie::setName(string name) { zName = name; }
+
+void Zombie::announce(void) {
+    cout << zName << ": BraiiiiiiinnnzzzZ..." << endl;
+}
--- ex00/src/randomChump.cpp
+++ ex00/src/randomChump.cpp
@@ -6,13 +6,13 @@
 /*   By: bkas <bkas@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 12:37:22 by bkas              #+#    #+#             */
-/*   Updated: 2024/05/23 14:04:55 by bkas             ###   ########.fr       */
+/*   Updated: 2024/05/23 14:07:06 by bkas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Zombie.hpp"
 
-void    randomChump(string name) {
+void randomChump(string name) {
     Zombie zm;
     zm.setName(name);
     zm.announce();
