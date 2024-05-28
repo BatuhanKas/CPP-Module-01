25782553975e36738b892f02f882036a4d3f72bc
example <email@example.com>
2024-05-28 22:32:38 +0300

    push

--- ex04/Stream.cpp
+++ ex04/Stream.cpp
@@ -6,26 +6,50 @@
 /*   By: bkas <bkas@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 16:57:30 by bkas              #+#    #+#             */
-/*   Updated: 2024/05/28 17:49:54 by bkas             ###   ########.fr       */
+/*   Updated: 2024/05/28 22:32:27 by bkas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Stream.hpp"
 
 void replaceProcess(string& filename, string& s1, string& s2) {
-    ifstream inputfile(filename);
-    ofstream outputfile(filename + ".replace");
+    ifstream inputfile(filename.c_str());
+    filename += ".replace";
+    ofstream outputfile(filename.c_str());
 
     string line;
+    size_t pos = 0, count = 0, tmp = 0;
     while (getline(inputfile, line)) {
+        cout << "line: " << line << endl;
+        cout << s1 << endl;
+        // tmp = line.find(s1, pos);
+        // cout << "1" << endl;
+        cout << "pos: " << pos << endl;
 
-        line.find(s1);
-
-
-
-
-        line == s1 ? outputfile << s2 << endl : outputfile << line << endl;
+        while ((tmp = line.find(s1, pos)) != string::npos) {
+            count++;
+            cout << "tmp:" << tmp << endl;
+            cout << "2" << endl;
+            // Aramaya kaldığı yerden devam et
+            pos += s1.length();
+            // pos = line.find(s1, pos);
+        }
+        pos = 0;
+        tmp = 0;
+        if (count) {
+            while (count) {
+                outputfile << s2;
+                count--;
+            }
+        } else {
+            outputfile << line;
+        }
+        outputfile << endl;
     }
+
+    cout << "melih size: " << count << endl;
+
+    line == s1 ? outputfile << s2 << endl : outputfile << line << endl;
 
     // batubatubatubatu
     // melihmelihmelihmelih
--- ex04/asd.cpp
+++ ex04/asd.cpp
@@ -4,17 +4,26 @@ using namespace std;
 
 // Driver code
 int main() {
-    string str = "geeksforgeeks a computer science";
-    string str1 = "sci";
+    string str = "asdfmelihmelihmelihmelih";
+    string str1 = "melih";
 
     // Find first occurrence of "geeks"
     size_t found = str.find(str1);
+    cout << "found: " << found << endl;
     string str2 = &str[found];
     cout << str2 << endl;
 
+
+
+
+
+    /**
+     * @brief kalsin
+     * 
     while ((pos = line.find(word, pos)) != std::string::npos) {
         ++count;
         // Aramaya kaldığı yerden devam et
         pos += word.length();
     }
+     */
 }
