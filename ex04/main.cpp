e4145cd83e018109c8249c455f7e18bb7172ffbb
Batuhan Kas <bkas@k2m04s04.42kocaeli.com.tr>
2024-05-29 18:04:23 +0300

    push

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,21 +6,21 @@
 /*   By: bkas <bkas@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 15:56:46 by bkas              #+#    #+#             */
-/*   Updated: 2024/05/29 17:51:43 by bkas             ###   ########.fr       */
+/*   Updated: 2024/05/29 18:03:52 by bkas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Stream.hpp"
 
-int main() {
-    string filename, s1, s2;
-
-    cout << "Enter filename:" << endl;
-    cin >> filename;
-    cout << "Enter Replace to String:" << endl;
-    cin >> s1;
-    cout << "Enter Replacement String:" << endl;
-    cin >> s2;
-
-    replaceProcess(filename, s1, s2);
+int main(int ac, char **av) {
+    if (ac == 4) {
+        string filename, s1, s2;
+        filename = av[1];
+        s1 = av[2];
+        s2 = av[3];
+        replaceProcess(filename, s1, s2);
+    } else {
+        cout << "Wrong Input! Need 3 Parameters!" << endl;
+    }
+    return 0;
 }
