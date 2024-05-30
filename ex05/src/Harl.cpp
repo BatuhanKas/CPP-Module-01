32c8846a617c3f49611eadddfaa25a0b498c75e5
Batuhan Kas <bkas@k2m02s04.42kocaeli.com.tr>
2024-05-30 18:19:10 +0300

    push

--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: bkas <bkas@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 10:41:26 by bkas              #+#    #+#             */
-/*   Updated: 2024/05/30 14:23:57 by bkas             ###   ########.fr       */
+/*   Updated: 2024/05/30 18:09:37 by bkas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,26 @@ void Harl::debug(void) {
     cout << "I love having extra bacon for my "
             "7XL-double-cheese-triple-pickle-special-ketchup burger."
          << endl;
-    cout << "I really do !" << endl;
+    cout << "I really do !" << endl << endl;
 }
 
 void Harl::info(void) {
     cout << "I cannot believe adding extra bacon costs more money." << endl;
-    cout << " You didn't put enough bacon in my burger !" << endl;
-    cout << "If you did, I wouldn't be asking for more!" << endl;
+    cout << "You didn't put enough bacon in my burger !" << endl;
+    cout << "If you did, I wouldn't be asking for more!" << endl << endl;
 }
 
 void Harl::warning(void) {
     cout << "I think I deserve to have some extra bacon for free." << endl;
     cout << "I've been coming for years whereas you started working here since "
             "last month."
+         << endl
          << endl;
 }
 
 void Harl::error(void) {
-    cout << "This is unacceptable! I want to speak to the manager now." << endl;
+    cout << "This is unacceptable! I want to speak to the manager now." << endl
+         << endl;
 }
 
 void Harl::complain(string level) {
